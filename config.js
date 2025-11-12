c6879753400fd147ce5fb287328ad2b37cd4aa89
nn <205666415+guessmesome@users.noreply.github.com>
2025-11-12 12:24:18 +0700

    upd

---
+++ config.js
@@ -0,0 +1,8 @@
+const CONFIG = {
+    products: {
+        1: 'https://trnsfx.ru/go/simdtm2g9g',
+        2: 'https://trnsfx.ru/go/skdugav7x0',
+        3: 'https://trnsfx.ru/go/simkq2c141',
+        4: 'https://trnsfx.ru/go/sgs40lsy4m'
+    }
+};
